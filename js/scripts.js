4462ec6045cca77e26da7d439d1610b992f96d4b
raitoaez <rafael.anez.sa@gmail.com>
2015-10-20 13:55:33 -0400

    otra version màs

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,51 @@
+ var tiempoEspera = 2000;
  $(document).ready(function() {
+ 	//var arboles = [];
+ 	for (var i = 1; i <= 100; i++) {
+ 		$(".cielo").append(getArbol(i));
+ 	};
+ 	for (var i = 1; i <= 100; i++) {
+ 		tiempoEspera += getTimeout();
+ 		mover(i,tiempoEspera);
+ 	};
+
      $(document).keydown(function(e) {
          if (e.keyCode == 38 || e.keyCode == 32) {
-             $(".man").fadeIn()
-                 .animate({
-                     bottom: 125
-                 }, 1000)
-                 .animate({
-                     bottom: 0
-                 }, 1000);
+             saltar();
          }
      });
  });
+
+ function saltar() {
+     $(".man").fadeIn()
+         .animate({
+             bottom: 210
+         }, 250)
+         .animate({
+             bottom: 0
+         }, 450);
+ }
+
+ function moverArbol(numeroArbol) {
+ 	var numeroArbol = '#arbol' + numeroArbol;
+	$(numeroArbol).fadeIn()
+         .animate({
+             left: -100
+         }, 4000);
+ }
+
+ function mover(numeroArbol, timeout) {
+ 	setTimeout(moverArbol(numeroArbol), timeout);
+ }
+
+ function getArbol(numero) {
+ 	var alto = randomIntFromInterval(50, 125)
+ 	return '<div id="arbol' + numero +'" class="arbol" style="height: ' + alto +'px"></div>';
+ }
+
+ function getTimeout() {
+ 	return randomIntFromInterval(500, 2000);
+ }
+ function randomIntFromInterval(min, max) {
+     return Math.floor(Math.random() * (max - min + 1) + min);
+ }
